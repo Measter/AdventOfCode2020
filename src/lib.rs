030da1cb0d5b422ef3281ba281105d02784a82e8
Stuart Haidon <serayen.sh@gmail.com>
2020-12-04 07:00:58 +0000

    Run part multiple times to estimated runtime.

--- src/lib.rs
+++ src/lib.rs
@@ -23,13 +23,24 @@ where
     for (part, i) in parts.iter().zip(1..) {
         println!("-- Part {} --", i);
 
-        let now = Instant::now();
         let part_result = part(input).with_context(|| eyre!("Error running Part {}", i))?;
-        let time = now.elapsed();
 
         println!("Result: {}", part_result);
 
-        let total_runs = (1.0 / time.as_secs_f64())
+        // Run a few times to get an estimate of how long it takes.
+        let mut min_run = Duration::from_secs(u64::MAX);
+
+        for _ in 0..5 {
+            let now = Instant::now();
+            let _ = part(input);
+            let time = now.elapsed();
+
+            if time < min_run {
+                min_run = time;
+            }
+        }
+
+        let total_runs = (1.0 / min_run.as_secs_f64())
             .ceil()
             .max(10.0)
             .min(u32::MAX as f64) as u32; // I doubt we'll actually *do* 4.2 billion runs...
