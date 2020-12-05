bc7c3b6f6d3196a3402cadc60cbdff57dda5b3dc
Stuart Haidon <serayen.sh@gmail.com>
2020-12-05 08:44:16 +0000

    Implemented Day 5.

---
+++ src/bin/aoc_2005.rs
@@ -0,0 +1,106 @@
+use std::collections::BTreeSet;
+
+use advent_of_code_2020::run;
+use color_eyre::eyre::{eyre, Report, Result};
+
+fn get_seat_row(input: &str) -> Result<(u16, u16)> {
+    let (row, seat) = input.split_at(7);
+
+    let row = row
+        .chars()
+        .map(|c| match c {
+            'F' => Ok(0),
+            'B' => Ok(1),
+            _ => Err(eyre!("Unexpected character: {}", c)),
+        })
+        .try_fold(0, |acc, digit| Ok::<_, Report>(acc * 2 + digit?))?;
+
+    let seat = seat
+        .chars()
+        .map(|c| match c {
+            'L' => Ok(0),
+            'R' => Ok(1),
+            _ => Err(eyre!("Unexpected character: {}", c)),
+        })
+        .try_fold(0, |acc, digit| Ok::<_, Report>(acc * 2 + digit?))?;
+
+    Ok((row, seat))
+}
+
+fn seat_id((row, seat): (u16, u16)) -> u16 {
+    row * 8 + seat
+}
+
+fn part1(input: &str) -> Result<u16> {
+    let mut max = 0;
+
+    let ids = input
+        .lines()
+        .map(str::trim)
+        .map(|l| get_seat_row(l).map(seat_id));
+
+    for id in ids {
+        max = max.max(id?);
+    }
+
+    Ok(max)
+}
+
+struct DebugToDisplay<T: std::fmt::Debug>(T);
+
+impl<T: std::fmt::Debug> std::fmt::Display for DebugToDisplay<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
+
+fn part2(input: &str) -> Result<DebugToDisplay<BTreeSet<u16>>> {
+    let mut seats: BTreeSet<u16> = (0..=1023).collect();
+
+    let ids = input
+        .lines()
+        .map(str::trim)
+        .map(|l| get_seat_row(l).map(seat_id));
+
+    for id in ids {
+        seats.remove(&id?);
+    }
+
+    Ok(DebugToDisplay(seats))
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let input = std::fs::read_to_string("inputs/aoc_2005.txt")?;
+
+    run("Day 5: Binary Boarding", &*input, &part1, &part2)
+}
+
+#[cfg(test)]
+mod tests_2005 {
+    use super::*;
+
+    #[test]
+    fn decode_test() {
+        let input = "FBFBBFFRLR";
+        let expected = (44, 5);
+
+        assert_eq!(get_seat_row(input).unwrap(), expected);
+    }
+
+    #[test]
+    fn part1_example() {
+        let tests = [
+            ("BFFFBBFRRR", (70, 7), 567),
+            ("FFFBBBFRRR", (14, 7), 119),
+            ("BBFFBBFRLL", (102, 4), 820),
+        ];
+
+        for (i, (test, expected_seat, expected_id)) in tests.iter().enumerate() {
+            let seat_row = get_seat_row(test).unwrap();
+            assert_eq!(seat_row, *expected_seat, "{}", i);
+            assert_eq!(seat_id(seat_row), *expected_id, "{}", i);
+        }
+    }
+}
