cf5083b950e8d195086f959f8dff16a9085bf301
Stuart Haidon <serayen.sh@gmail.com>
2021-01-08 06:50:56 +0000

    Make day 17 generic over dimensions.

--- src/bin/aoc_2017.rs
+++ src/bin/aoc_2017.rs
@@ -1,187 +1,114 @@
-use std::{collections::HashSet, ops::RangeInclusive};
+use std::collections::HashSet;
 
 use aoc_lib::TracingAlloc;
-use color_eyre::eyre::Result;
+use color_eyre::{eyre::Result, Report};
 use itertools::Itertools;
 
 #[global_allocator]
 static ALLOC: TracingAlloc = TracingAlloc::new();
 
-type Field = HashSet<(i32, i32, i32, i32)>;
+fn parse_3d(input: &str) -> Result<HashSet<[i8; 3]>> {
+    let mut state = HashSet::new();
 
-#[derive(Debug, Clone, Copy, Default)]
-struct Bounds {
-    lower: i32,
-    upper: i32,
+    for (line, y) in input.lines().zip(0..) {
+        for (_, x) in line.chars().zip(0..).filter(|(c, _)| *c == '#') {
+            state.insert([x, y, 0]);
+        }
+    }
+
+    Ok(state)
 }
 
-impl Bounds {
-    fn expand(&mut self) {
-        self.lower -= 1;
-        self.upper += 1;
+fn get_neighbours_3d([x, y, z]: [i8; 3]) -> impl Iterator<Item = [i8; 3]> {
+    let x_range = -1..=1;
+    let y_range = -1..=1;
+    let z_range = -1..=1;
+
+    x_range
+        .cartesian_product(y_range)
+        .cartesian_product(z_range)
+        .filter(|&((x, y), z)| [x, y, z] != [0, 0, 0])
+        .map(move |((rx, ry), rz)| [x + rx, y + ry, z + rz])
+}
+
+fn parse_4d(input: &str) -> Result<HashSet<[i8; 4]>> {
+    let mut state = HashSet::new();
+
+    for (line, y) in input.lines().zip(0..) {
+        for (_, x) in line.chars().zip(0..).filter(|(c, _)| *c == '#') {
+            state.insert([x, y, 0, 0]);
+        }
     }
 
-    fn range(self) -> RangeInclusive<i32> {
-        self.lower..=self.upper
-    }
+    Ok(state)
+}
+
+fn get_neighbours_4d([x, y, z, w]: [i8; 4]) -> impl Iterator<Item = [i8; 4]> {
+    let x_range = -1..=1;
+    let y_range = -1..=1;
+    let z_range = -1..=1;
+    let w_range = -1..=1;
+
+    x_range
+        .cartesian_product(y_range)
+        .cartesian_product(z_range)
+        .cartesian_product(w_range)
+        .filter(|&(((x, y), z), w)| [x, y, z, w] != [0, 0, 0, 0])
+        .map(move |(((rx, ry), rz), rw)| [x + rx, y + ry, z + rz, w + rw])
 }
 
 #[derive(Debug, Clone)]
-struct GameField {
-    state: Field,
-    buf: Field,
-    x_bounds: Bounds,
-    y_bounds: Bounds,
-    z_bounds: Bounds,
-    w_bounds: Bounds,
+struct GameField<T, F> {
+    state: HashSet<T>,
+    buf: HashSet<T>,
+    check_buf: HashSet<T>,
+    neighbour_func: F,
 }
 
-impl GameField {
-    fn parse(input: &str) -> Result<GameField> {
-        let mut x_bounds = Bounds::default();
-        let mut y_bounds = Bounds::default();
+impl<T, F, I> GameField<T, F>
+where
+    T: Copy + Eq + std::hash::Hash,
+    F: Fn(T) -> I,
+    I: Iterator<Item = T>,
+{
+    fn new(state: HashSet<T>, neighbour_func: F) -> Self {
+        Self {
+            state,
+            buf: HashSet::new(),
+            check_buf: HashSet::new(),
+            neighbour_func,
+        }
+    }
 
-        let mut state = Field::new();
+    fn step(&mut self) {
+        // So we don't have multiple mutable borrows through self.
+        let Self {
+            buf,
+            check_buf,
+            state,
+            ..
+        } = self;
 
-        for (line, y) in input.lines().zip(0..) {
-            y_bounds.upper = y;
+        buf.clear();
+        check_buf.clear();
+        check_buf.extend(&*state);
 
-            for (_, x) in line.chars().zip(0..).filter(|(c, _)| *c == '#') {
-                x_bounds.upper = x;
-                state.insert((x, y, 0, 0));
+        for node in &*state {
+            check_buf.extend((self.neighbour_func)(*node));
+        }
+
+        for &node in &*check_buf {
+            let living_neighours = (self.neighbour_func)(node)
+                .filter(|neighbour| state.contains(neighbour))
+                .take(4)
+                .count();
+
+            let alive = state.contains(&node);
+
+            if matches!((alive, living_neighours), (true, 2..=3) | (false, 3)) {
+                buf.insert(node);
             }
         }
-
-        Ok(GameField {
-            state,
-            buf: Field::new(),
-            x_bounds,
-            y_bounds,
-            z_bounds: Bounds::default(),
-            w_bounds: Bounds::default(),
-        })
-    }
-
-    fn count_neighbours_3d(&self, x: i32, y: i32, z: i32) -> usize {
-        let x_range = -1..=1;
-        let y_range = -1..=1;
-        let z_range = -1..=1;
-
-        x_range
-            .cartesian_product(y_range)
-            .cartesian_product(z_range)
-            .map(|((rx, ry), rz)| (x + rx, y + ry, z + rz))
-            .filter(|&(rx, ry, rz)| !(rx == x && ry == y && rz == z))
-            .flat_map(|(x, y, z)| self.state.get(&(x, y, z, 0)))
-            .take(4)
-            .count()
-    }
-
-    fn step_3d(&mut self) {
-        self.buf.clear();
-
-        self.x_bounds.expand();
-        self.y_bounds.expand();
-        self.z_bounds.expand();
-
-        let coords = self
-            .x_bounds
-            .range()
-            .cartesian_product(self.y_bounds.range())
-            .cartesian_product(self.z_bounds.range());
-
-        let mut new_x_bounds = Bounds::default();
-        let mut new_y_bounds = Bounds::default();
-        let mut new_z_bounds = Bounds::default();
-
-        for ((x, y), z) in coords {
-            let neighbours = self.count_neighbours_3d(x, y, z);
-
-            let cube = self.state.contains(&(x, y, z, 0));
-
-            match (cube, neighbours) {
-                (true, 2..=3) | (false, 3) => {
-                    self.buf.insert((x, y, z, 0));
-                    new_x_bounds.lower = new_x_bounds.lower.min(x);
-                    new_x_bounds.upper = new_x_bounds.upper.max(x);
-                    new_y_bounds.lower = new_y_bounds.lower.min(y);
-                    new_y_bounds.upper = new_y_bounds.upper.max(y);
-                    new_z_bounds.lower = new_z_bounds.lower.min(z);
-                    new_z_bounds.upper = new_z_bounds.upper.max(z);
-                }
-                _ => {}
-            };
-        }
-
-        self.x_bounds = new_x_bounds;
-        self.y_bounds = new_y_bounds;
-        self.z_bounds = new_z_bounds;
-
-        std::mem::swap(&mut self.buf, &mut self.state);
-    }
-
-    fn count_neighbours_4d(&self, x: i32, y: i32, z: i32, w: i32) -> usize {
-        let x_range = -1..=1;
-        let y_range = -1..=1;
-        let z_range = -1..=1;
-        let w_range = -1..=1;
-
-        x_range
-            .cartesian_product(y_range)
-            .cartesian_product(z_range)
-            .cartesian_product(w_range)
-            .map(|(((rx, ry), rz), rw)| (x + rx, y + ry, z + rz, w + rw))
-            .filter(|&(rx, ry, rz, rw)| !(rx == x && ry == y && rz == z && rw == w))
-            .flat_map(|(x, y, z, w)| self.state.get(&(x, y, z, w)))
-            .take(4)
-            .count()
-    }
-
-    fn step_4d(&mut self) {
-        self.buf.clear();
-
-        self.x_bounds.expand();
-        self.y_bounds.expand();
-        self.z_bounds.expand();
-        self.w_bounds.expand();
-
-        let coords = self
-            .x_bounds
-            .range()
-            .cartesian_product(self.y_bounds.range())
-            .cartesian_product(self.z_bounds.range())
-            .cartesian_product(self.w_bounds.range());
-
-        let mut new_x_bounds = Bounds::default();
-        let mut new_y_bounds = Bounds::default();
-        let mut new_z_bounds = Bounds::default();
-        let mut new_w_bounds = Bounds::default();
-
-        for (((x, y), z), w) in coords {
-            let neighbours = self.count_neighbours_4d(x, y, z, w);
-
-            let cube = self.state.contains(&(x, y, z, w));
-
-            match (cube, neighbours) {
-                (true, 2..=3) | (false, 3) => {
-                    self.buf.insert((x, y, z, w));
-                    new_x_bounds.lower = new_x_bounds.lower.min(x);
-                    new_x_bounds.upper = new_x_bounds.upper.max(x);
-                    new_y_bounds.lower = new_y_bounds.lower.min(y);
-                    new_y_bounds.upper = new_y_bounds.upper.max(y);
-                    new_z_bounds.lower = new_z_bounds.lower.min(z);
-                    new_z_bounds.upper = new_z_bounds.upper.max(z);
-                    new_w_bounds.lower = new_w_bounds.lower.min(w);
-                    new_w_bounds.upper = new_w_bounds.upper.max(w);
-                }
-                _ => {}
-            };
-        }
-
-        self.x_bounds = new_x_bounds;
-        self.y_bounds = new_y_bounds;
-        self.z_bounds = new_z_bounds;
-        self.w_bounds = new_w_bounds;
 
         std::mem::swap(&mut self.buf, &mut self.state);
     }
@@ -194,19 +121,27 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     let input = aoc_lib::input(2020, 17).open()?;
-    let (state, parse_bench) = aoc_lib::bench(&ALLOC, "Parse", &|| GameField::parse(&input))?;
+    let (game_3d, parse3d_bench) = aoc_lib::bench::<_, Report>(&ALLOC, "Parse 3D", &|| {
+        let state = parse_3d(&input)?;
+        Ok(GameField::new(state, get_neighbours_3d))
+    })?;
+    let (game_4d, parse4d_bench) = aoc_lib::bench::<_, Report>(&ALLOC, "Parse 4D", &|| {
+        let state = parse_4d(&input)?;
+        Ok(GameField::new(state, get_neighbours_4d))
+    })?;
+
     let (p1_res, p1_bench) = aoc_lib::bench::<_, ()>(&ALLOC, "Part 1", &|| {
-        let mut state = state.clone();
+        let mut state = game_3d.clone();
         for _ in 0..6 {
-            state.step_3d();
+            state.step();
         }
 
         Ok(state.count_active())
     })?;
     let (p2_res, p2_bench) = aoc_lib::bench::<_, ()>(&ALLOC, "Part 2", &|| {
-        let mut state = state.clone();
+        let mut state = game_4d.clone();
         for _ in 0..6 {
-            state.step_4d();
+            state.step();
         }
 
         Ok(state.count_active())
@@ -214,7 +149,12 @@ fn main() -> Result<()> {
 
     aoc_lib::display_results(
         "Day 17: Conway Cubes",
-        &[(&"", parse_bench), (&p1_res, p1_bench), (&p2_res, p2_bench)],
+        &[
+            (&"", parse3d_bench),
+            (&"", parse4d_bench),
+            (&p1_res, p1_bench),
+            (&p2_res, p2_bench),
+        ],
     );
 
     Ok(())
@@ -232,14 +172,15 @@ mod tests_2017 {
             .example(Example::Part1, 1)
             .open()
             .unwrap();
-        let mut state = GameField::parse(&input).unwrap();
+        let state = parse_3d(&input).unwrap();
+        let mut game = GameField::new(state, get_neighbours_3d);
 
         for _ in 0..6 {
-            state.step_3d();
+            game.step();
         }
 
         let expected = 112;
-        let actual = state.count_active();
+        let actual = game.count_active();
 
         assert_eq!(expected, actual);
     }
@@ -250,14 +191,15 @@ mod tests_2017 {
             .example(Example::Part1, 1)
             .open()
             .unwrap();
-        let mut state = GameField::parse(&input).unwrap();
+        let state = parse_4d(&input).unwrap();
+        let mut game = GameField::new(state, get_neighbours_4d);
 
         for _ in 0..6 {
-            state.step_4d();
+            game.step();
         }
 
         let expected = 848;
-        let actual = state.count_active();
+        let actual = game.count_active();
 
         assert_eq!(expected, actual);
     }
